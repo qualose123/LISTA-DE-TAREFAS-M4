cab328628cd0c8e061339861a7e801b67105991f
qualose123 <jonhyzicaos@gmail.com>
2022-08-08 21:46:45 -0300

    LOADING ao requisitar

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -2,18 +2,27 @@ import { useState, useEffect } from "react";
 import "./Home.css";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-function Home() {
-  const [tarefa, setTarefa] = useState([]);
+import Spinner from "react-bootstrap/Spinner";
+import Button from "react-bootstrap/Button";
 
-  //READ ALL
-  const getAllTarefas = () => {
-    fetch("http://localhost:8000/tarefas")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        setTarefa(json);
-      });
+function Home() {
+  //state do getall
+  const [tarefa, setTarefa] = useState([]);
+  //state do loading
+  const [loading, setLoading] = useState(false);
+
+  const getAllTarefas = async () => {
+    //aki faz com q o spinner apareça antes e durante a requisição a API
+    setLoading(true);
+    //diz q é uma func asyncrona
+    //faz requisição e espera a resposta, e guarda essa resposta
+    let response = await fetch("http://localhost:8000/tarefas");
+    //Espera a resposta e transforma ela em json e salva o mesmo na variavel
+    let json = await response.json();
+    //esse aki faz com que o spinner suma, após a requisição da API
+    setLoading(false);
+    //aki está a resposta json, aki faço o que quiser cm ela
+    setTarefa(json);
   };
 
   useEffect(() => {
@@ -22,29 +31,60 @@ function Home() {
 
   return (
     <div>
-      <div>
-        <h1> To Do List </h1>
-      </div>
-      <div className="container">
-        {tarefa.map((tarefa, index) => (
-          <div className="C-Card" key={index}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={tarefa.imagem} />
-              <Card.Body>
-                <Card.Title>{tarefa.title}</Card.Title>
-                <Card.Text>{tarefa.description}</Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>{tarefa.objective}</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="#">Editar</Card.Link>
-                <Card.Link href="#">Deletar</Card.Link>
-              </Card.Body>
-            </Card>
+      {/* Esse Loading abaixo exibe um spinner de giratorio com a palavra loading, enquanto o site estiver requisitando */}
+      {loading && (
+        <div>
+          <Button variant="primary" disabled>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            <span className="visually-hidden">Loading...</span>
+          </Button>{" "}
+          <Button variant="primary" disabled>
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            Loading...
+          </Button>
+        </div>
+      )}
+      {/*Esse Loading, esconde o spinner loading após a requisição está completa e exibe o código abaixo*/}
+      {!loading && (
+        <>
+          <div>
+            <h1> To Do List </h1>
           </div>
-        ))}
-      </div>
+
+          <div className="container">
+            {tarefa.map((tarefa, index) => (
+              <div className="C-Card" key={index}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={tarefa.imagem} />
+                  <Card.Body>
+                    <Card.Title>{tarefa.title}</Card.Title>
+                    <Card.Text>{tarefa.description}</Card.Text>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{tarefa.objective}</ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href="#">Editar</Card.Link>
+                    <Card.Link href="#">Deletar</Card.Link>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
