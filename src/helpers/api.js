1cfd8552ba21e4b2f8f8705ffa240b8021c61111
qualose123 <jonhyzicaos@gmail.com>
2022-08-23 16:37:04 -0300

    UP EM API HELPER

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,16 +1,15 @@
 const tarefaContext = {
-    tarefaEndpoint: () => `${Api.baseUrl}/tarefas`,
-    tarefa: () => tarefaContext.tarefaEndpoint(),
-    tarefaById: (id) => `${tarefaContext.tarefaEndpoint()}/${id}`,
-  };
-  
-  const urls = {
-    production: "https://api-fake.herokuapp.com/",
-    development: "http://localhost:8000",
-  };
-  export const Api = {
-    // baseUrl: "http://localhost:8000",
-    baseUrl: urls[process.env.NODE_ENV],
-    ...tarefaContext,
-  };
-  
+  tarefaEndpoint: () => `${Api.baseUrl}/tarefas`,
+  tarefa: () => tarefaContext.tarefaEndpoint(),
+  tarefaById: (id) => `${tarefaContext.tarefaEndpoint()}/${id}`,
+};
+
+const urls = {
+  production: "https://api-fake.herokuapp.com/",
+  development: "http://localhost:8000",
+};
+export const Api = {
+  // baseUrl: "http://localhost:8000",
+  baseUrl: urls[process.env.NODE_ENV],
+  ...tarefaContext,
+};
